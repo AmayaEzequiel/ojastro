687f755e2746eaa04b6861a834fdc7a01e8fa01a
EASilaba <eamaya@silaba.com>
2026-05-07 08:48:58 -0500

    cambios

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({build: {
+    assets: 'assets' // Esto agrupa todo en una carpeta llamada "assets"
+  },
+ });
---
+++ src/data/modelos.js
@@ -0,0 +1,129 @@
+
+
+export const misModelos = [
+    {
+        id: "C5",
+        brand: "OMODA", // Nueva propiedad
+        name: "C5",      // Limpiamos el nombre (antes era "Omoda C5")
+        badge: "NG",    // Antes variant
+        image: "./img/c5.png",
+        urlPage: "/modelos/c5",   // Nueva propiedad
+        urlSpecs: "/pdf/c5.pdf",  // Nueva propiedad
+        specs: [
+            "Transmisión automática en todas sus versiones",
+            "6 bolsas de aire",
+            "Sunroof",
+            "Cámara 540° HD",
+            "Asistente inteligente de voz Omoda",
+            "Función de masaje para el asiento del conductor",
+        ],
+    },
+    {
+        id: "C7",
+        brand: "OMODA",
+        name: "C7",
+        image: "./img/c7.png",
+        urlPage: "/c5",   // Nueva propiedad
+        urlSpecs: "/pdf/c5.pdf",  // Nueva propiedad
+        specs: [
+            "Motor de 1.6L Turbo",
+            "Asientos de cuero sintético",
+            "Pantalla de 15.6",
+            "Techo panorámico",
+            "Modo descanso",
+            "Vidrios con protección acústica",
+        ],
+    },
+
+
+
+    {
+        id: "J5",
+        brand: "JAECOO",
+        name: "J5",
+        image: "./img/j5.png",
+        urlPage: "/j5",   // Nueva propiedad
+        urlSpecs: "/pdf/j5.pdf",  // Nueva propiedad
+        specs: [
+            "Motor de 1.5L Turbo",
+            "6 bolsas de aire",
+            "Asientos deportivos de cuero sintético",
+            "Sistemas de asistencias a la conducción (ADAS)",
+            "Apple CarPlay y Android inalámbrico de fábrica",
+            "Asistente inteligente de voz Jaecoo",
+        ],
+    },
+
+
+    {
+        id: "J7",
+        brand: "JAECOO",
+        name: "J7",
+        badge: "",
+        image: "/img/j7.png",
+        urlPage: "/modelos/j7",
+        urlSpecs: "/pdf/j7.pdf",
+        specs: [
+            "Tracción All Wheel Drive, con 7 modos de manejo",
+            "7 bolsas de aire",
+            "Techo panorámico",
+            "Pantalla de Infoentretenimiento de 14.8”",
+            "Cámara 540º con vista panorámica desde arriba",
+            "Asistente inteligente de voz Jaecoo",
+        ],
+    },
+
+
+    {
+        id: "J7-SHS",
+        brand: "JAECOO",
+        name: "J7",
+        badge: "SHS",
+        image: "./img/j7shs.webp",
+        urlPage: "",
+        urlSpecs: "",
+        specs: [
+            "Tracción All Wheel Drive, con 7 modos de manejo",
+            "7 bolsas de aire",
+            "Techo panorámico",
+            "Pantalla de Infoentretenimiento de 14.8”",
+            "Cámara 540º con vista panorámica desde arriba",
+            "Asistente inteligente de voz Jaecoo",
+        ],
+    },
+   {
+        id: "J8",
+        brand: "JAECOO",
+        name: "J8",
+        badge: "",
+        image: "./img/j8.png",
+        urlPage: "/j8",
+        urlSpecs: "pdf/j8.pdf",
+        specs: [
+            "3 filas de asientos",
+            "Cámara 540º con vista panorámica desde arriba",
+            "Asientos deportivos de cuero sintético",
+            "12 bocinas Sony",
+            "Sistemas de asistencias a la conducción (ADAS)",
+            "Asistente inteligente de voz Jaecoo",
+        ],
+    },
+
+ {
+        id: "J8 SHS",
+        brand: "JAECOO",
+        name: "J8",
+        badge: "SHS",
+        image: "./img/j8shs.webp",
+        urlPage: "",
+        urlSpecs: "",
+        specs: [
+            "Motor híbrido de 1.5L Turbo",
+            "Hasta 1,400 km de autonomía combinada",
+            "3 filas de asientos",
+            "7 bolsas de aire",
+            "Sistemas de Asistencias al Manejo (ADAS)",
+            "Tracción en las cuatro ruedas, All Wheel Drive",
+        ],
+    },
+];
